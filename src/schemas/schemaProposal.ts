2e271a45ef6f47bee372bb79625f5ee64f29f5b1
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-08-06 14:56:06 -0300

    feat: update schemaProposal

--- src/schemas/schemaProposal.ts
+++ src/schemas/schemaProposal.ts
@@ -4,6 +4,7 @@ import { ProposalData } from '../interfaces/index.js';
 
 const regexIATA = /^[a-zA-ZáéíóúàâêôãõüçÁÉÍÓÚÀÂÊÔÃÕÜÇ ]+\s-\s[A-Z]{2,3}$/;
 const regexData = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+const regexCity = /^[a-zA-ZáéíóúàâêôãõüçÁÉÍÓÚÀÂÊÔÃÕÜÇ ]+$/;
 
 const schemaProposal: joi.ObjectSchema<ProposalData> = joi.object({
     companyId: joi.number().integer().required(),
@@ -13,7 +14,7 @@ const schemaProposal: joi.ObjectSchema<ProposalData> = joi.object({
     start: joi.string().pattern(regexData).required(),
     end: joi.string().pattern(regexData).required(),
     name: joi.string().required(),
-    destiny: joi.string().required(),
+    destiny: joi.string().pattern(regexCity).required(),
     type: joi.equal(
         'Travel', 'Exchange', 'Backpack', 'Voluntary'
     ).required(),
