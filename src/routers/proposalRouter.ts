2b0c5e0d17c2b3720009e6a3bc09bc8dd40a0768
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-08-06 00:30:16 -0300

    feat: schema proposal and setup router proposal

--- src/routers/proposalRouter.ts
+++ src/routers/proposalRouter.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 
+import authUserOrCompany from "../middlewares/authMiddleware.js";
+import validationSchema from "../middlewares/schemaMiddleware.js";
+import schemaProposal from "../schemas/schemaProposal.js";
+
 const proposalRouter = Router();
 
-proposalRouter.post('/proposal');
-proposalRouter.post('/company/proposals');
-proposalRouter.get('/user/proposals');
+proposalRouter.post('/proposal', authUserOrCompany, validationSchema(schemaProposal));
+proposalRouter.get('/company/proposals', authUserOrCompany);
+proposalRouter.get('/user/proposals', authUserOrCompany);
 
 export default proposalRouter;
--- src/schemas/schemaProposal.ts
+++ src/schemas/schemaProposal.ts
@@ -1,6 +1,20 @@
 import joi from 'joi';
 
-const schemaProposal: joi.ObjectSchema = joi.object({
+import { ProposalData } from '../interfaces/index.js';
+
+const schemaProposal: joi.ObjectSchema<ProposalData> = joi.object({
+    companyId: joi.number().integer().required(),
+    airline: joi.string().required(),
+    boarding: joi.string().required(),
+    landing: joi.string().required(),
+    start: joi.string().required().trim(),
+    end: joi.string().required().trim(),
+    name: joi.string().required(),
+    destiny: joi.string().required(),
+    type: joi.equal(
+        'Travel', 'Exchange', 'Backpack', 'Voluntary'
+    ).required(),
+    year: joi.string().length(4).required().trim()
 });
 
 export default schemaProposal;
