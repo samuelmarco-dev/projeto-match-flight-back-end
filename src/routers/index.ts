bbdb478ae1e41d4547d016a496fabdca6c28e162
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-08-06 00:56:35 -0300

    feat: setup router in proposal controller

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import userRouter from './userRouter.js';
 import companyRouter from './companyRouter.js';
+import proposalRouter from './proposalRouter.js';
 
 const router = Router();
 
 router.use('/user', userRouter);
 router.use('/company', companyRouter);
+router.use(proposalRouter);
 
 export default router;
--- src/routers/proposalRouter.ts
+++ src/routers/proposalRouter.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 
+import { createProposal, getProposalsByCompany, getProposalsCompany } from "../controllers/proposalController.js";
 import authUserOrCompany from "../middlewares/authMiddleware.js";
 import validationSchema from "../middlewares/schemaMiddleware.js";
 import schemaProposal from "../schemas/schemaProposal.js";
 
 const proposalRouter = Router();
 
-proposalRouter.post('/proposal', authUserOrCompany, validationSchema(schemaProposal));
-proposalRouter.get('/company/proposals', authUserOrCompany);
-proposalRouter.get('/user/proposals', authUserOrCompany);
+proposalRouter.post('/proposal', authUserOrCompany, validationSchema(schemaProposal), createProposal);
+proposalRouter.get('/company/proposals', authUserOrCompany, getProposalsByCompany);
+proposalRouter.get('/user/proposals', authUserOrCompany, getProposalsCompany);
 
 export default proposalRouter;
