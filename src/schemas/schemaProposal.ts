73289e7df78017e6a58df6f107f540eb32e4f4a9
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-08-06 13:10:11 -0300

    feat: schemaProposal in request with standardized regex

--- src/schemas/schemaProposal.ts
+++ src/schemas/schemaProposal.ts
@@ -2,13 +2,16 @@ import joi from 'joi';
 
 import { ProposalData } from '../interfaces/index.js';
 
+const regexIATA = /^[a-zA-ZáéíóúàâêôãõüçÁÉÍÓÚÀÂÊÔÃÕÜÇ ]+\s-\s[A-Z]{2,3}$/;
+const regexData = /^[0-9]{2}\/[0-9]{2}\/[0-9]{4}$/;
+
 const schemaProposal: joi.ObjectSchema<ProposalData> = joi.object({
     companyId: joi.number().integer().required(),
-    airline: joi.string().required(),
-    boarding: joi.string().required(),
-    landing: joi.string().required(),
-    start: joi.string().required().trim(),
-    end: joi.string().required().trim(),
+    airline: joi.string().pattern(regexIATA).required(),
+    boarding: joi.string().pattern(regexIATA).required(),
+    landing: joi.string().pattern(regexIATA).required(),
+    start: joi.string().pattern(regexData).required(),
+    end: joi.string().pattern(regexData).required(),
     name: joi.string().required(),
     destiny: joi.string().required(),
     type: joi.equal(
