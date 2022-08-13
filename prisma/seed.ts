0789ee4a665a26e629d2083fd243abc7a9d039f5
Samuel Marco <samuelmarco.2020@alunos.utfpr.edu.br>
2022-08-13 01:28:18 -0300

    feat: populating test database to validate integration tests with db seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,1 +1,14 @@
-//arquivo para popular o banco de dados para facilitar os testes
+import prisma from "../src/config/database.js";
+import populatingDataBase from "./populatingDataBase.js";
+
+async function main(){
+    await populatingDataBase.createImages();
+    await populatingDataBase.createUsers();
+}
+
+main().catch(error => {
+    console.log(error);
+    process.exit(1);
+}).finally(async() => {
+    await prisma.$disconnect();
+});
